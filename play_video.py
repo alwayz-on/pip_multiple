8957e943ec7acdaabe7352e069040ae5c9cc857f
Ramy Al Sharif <ramyalsharif.tijdelijk@outlook.com>
2020-11-14 14:56:06 +0100

    first commit

---
+++ play_video.py
@@ -0,0 +1,186 @@
+import pafy
+import sys
+import vlc
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+
+
+class SideGrip(QWidget):
+    def __init__(self, parent, edge):
+        QWidget.__init__(self, parent)
+        if edge == Qt.LeftEdge:
+            self.setCursor(Qt.SizeHorCursor)
+            self.resizeFunc = self.resizeLeft
+        elif edge == Qt.TopEdge:
+            self.setCursor(Qt.SizeVerCursor)
+            self.resizeFunc = self.resizeTop
+        elif edge == Qt.RightEdge:
+            self.setCursor(Qt.SizeHorCursor)
+            self.resizeFunc = self.resizeRight
+        else:
+            self.setCursor(Qt.SizeVerCursor)
+            self.resizeFunc = self.resizeBottom
+        self.mousePos = None
+
+    def resizeLeft(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() - delta.x())
+        geo = window.geometry()
+        geo.setLeft(geo.right() - width)
+        window.setGeometry(geo)
+
+    def resizeTop(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() - delta.y())
+        geo = window.geometry()
+        geo.setTop(geo.bottom() - height)
+        window.setGeometry(geo)
+
+    def resizeRight(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() + delta.x())
+        window.resize(width, window.height())
+
+    def resizeBottom(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() + delta.y())
+        window.resize(window.width(), height)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.mousePos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.mousePos is not None:
+            delta = event.pos() - self.mousePos
+            self.resizeFunc(delta)
+
+    def mouseReleaseEvent(self, event):
+        self.mousePos = None
+
+
+class MainWindow(QMainWindow):
+    _gripSize = 1
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs, )
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        self.sideGrips = [
+            SideGrip(self, Qt.LeftEdge),
+            SideGrip(self, Qt.TopEdge),
+            SideGrip(self, Qt.RightEdge),
+            SideGrip(self, Qt.BottomEdge),
+        ]
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        # corner grips should be "on top" of everything, otherwise the side grips
+        # will take precedence on mouse events, so we are adding them *after*;
+        # alternatively, widget.raise_() can be used
+        self.cornerGrips = [QSizeGrip(self) for i in range(4)]
+        self.sizeHint = lambda: QSize(720, 480)
+        self.move(100, 10)
+
+        self.videoFrame = QFrame()
+        self.setCentralWidget(self.videoFrame)
+
+        self.vlcInstance = vlc.Instance(
+            ['--video-on-top', '--input-repeat=9999'])
+        self.videoPlayer = self.vlcInstance.media_player_new()
+        url, ok = QInputDialog.getText(
+            self, 'Text Input Dialog', 'Enter video link:')
+
+        # creating pafy object of the video
+        video = pafy.new(url)
+
+        # getting best stream
+        best = video.getbest()
+
+        playurl = best.url
+
+        media = self.vlcInstance.media_new(playurl)
+        media.get_mrl()
+        self.videoPlayer.set_media(media)
+
+        self.videoPlayer.audio_set_mute(True)
+        if sys.platform.startswith('linux'):  # for Linux using the X Server
+            self.videoPlayer.set_xwindow(self.videoFrame.winId())
+        elif sys.platform == "win32":  # for Windows
+            self.videoPlayer.set_hwnd(self.videoFrame.winId())
+        elif sys.platform == "darwin":  # for MacOS
+            self.videoPlayer.set_nsobject(int(self.videoFrame.winId()))
+        # self.vlcInstance.vlm_set_loop(media, True)
+        self.videoPlayer.play()
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint(event.globalPos() - self.oldPos)
+        # print(delta)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+
+    @property
+    def gripSize(self):
+        return self._gripSize
+
+    def setGripSize(self, size):
+        if size == self._gripSize:
+            return
+        self._gripSize = max(2, size)
+        self.updateGrips()
+
+    def updateGrips(self):
+        self.setContentsMargins(*[self.gripSize] * 4)
+
+        outRect = self.rect()
+        # an "inner" rect used for reference to set the geometries of size grips
+        inRect = outRect.adjusted(self.gripSize, self.gripSize,
+                                  -self.gripSize, -self.gripSize)
+
+        # top left
+        self.cornerGrips[0].setGeometry(
+            QRect(outRect.topLeft(), inRect.topLeft()))
+        # top right
+        self.cornerGrips[1].setGeometry(
+            QRect(outRect.topRight(), inRect.topRight()).normalized())
+        # bottom right
+        self.cornerGrips[2].setGeometry(
+            QRect(inRect.bottomRight(), outRect.bottomRight()))
+        # bottom left
+        self.cornerGrips[3].setGeometry(
+            QRect(outRect.bottomLeft(), inRect.bottomLeft()).normalized())
+
+        # left edge
+        self.sideGrips[0].setGeometry(
+            0, inRect.top(), self.gripSize, inRect.height())
+        # top edge
+        self.sideGrips[1].setGeometry(
+            inRect.left(), 0, inRect.width(), self.gripSize)
+        # right edge
+        self.sideGrips[2].setGeometry(
+            inRect.left() + inRect.width(),
+            inRect.top(), self.gripSize, inRect.height())
+        # bottom edge
+        self.sideGrips[3].setGeometry(
+            self.gripSize, inRect.top() + inRect.height(),
+            inRect.width(), self.gripSize)
+
+    def resizeEvent(self, event):
+        QMainWindow.resizeEvent(self, event)
+        self.updateGrips()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setApplicationName("VLC Test")
+
+    window = MainWindow()
+    app.exec_()
